8146d052b3a0447a0817960d8607c7d9850ebbba
Owner <Owner@DESKTOP-TOGCH03>
2019-04-08 18:10:58 +0300

    First Upload

---
+++ ProjectJava/src/Rest.java
@@ -0,0 +1,9 @@
+//MAIN CLASS WHERE main FUNCTION IS
+public class Rest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hellow world");
+	}
+
+}
