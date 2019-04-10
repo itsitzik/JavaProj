cb3a752eba51caf268381f83f46d64118f700428
Owner <Owner@DESKTOP-TOGCH03>
2019-04-10 20:52:38 +0300

    use xml

--- ProjectJava/src/Rest.java
+++ ProjectJava/src/Rest.java
@@ -1,9 +1,24 @@
+import javax.swing.*; 
+import javax.xml.parsers.*;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import java.io.*;
+
 //MAIN CLASS WHERE main FUNCTION IS
 public class Rest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("hellow world");
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+		File inputFile = new File(".settings/RestSettings.xml");
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document doc = builder.parse(inputFile);
+		System.out.println(doc.getDocumentElement().getNodeName());
+	}
+	
+	void InitRest() {
+		JFrame gui = new JFrame();
+		
 	}
 
 }
